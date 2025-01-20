f606f119ecb5c04ab06654589ec14f4506bdf131
denischzhen <den.chzhen@gmail.com>
2025-01-20 15:07:15 +0300

    Практическая работа 1 часть 1 завершена (убраны try catch) добавлен скролл результата)

--- 122_Chzhen/MainWindow.xaml.cs
+++ 122_Chzhen/MainWindow.xaml.cs
@@ -8,58 +8,71 @@ namespace _122_Chzhen
         public MainWindow()
         {
             InitializeComponent();
+            this.Closing += Window_Closing;
         }
 
         private void CalculateButton_Click(object sender, RoutedEventArgs e)
         {
-            try
+            double x, y;
+
+            // Проверка на валидность ввода
+            if (!double.TryParse(InputX.Text, out x) || !double.TryParse(InputY.Text, out y))
             {
-                double x = double.Parse(InputX.Text);
-                double y = double.Parse(InputY.Text);
-                double f_x = 0;
-
-                // Выбор функции
-                if (FuncSh.IsChecked == true)
-                {
-                    f_x = Math.Sinh(x);
-                }
-                else if (FuncX2.IsChecked == true)
-                {
-                    f_x = Math.Pow(x, 2);
-                }
-                else if (FuncExp.IsChecked == true)
-                {
-                    f_x = Math.Exp(x);
-                }
-
-                // Вычисление d
-                double d;
-                if (x > y)
-                {
-                    d = Math.Pow(f_x - y, 3) + Math.Atan(f_x);
-                }
-                else if (x < y)
-                {
-                    d = Math.Pow(y - f_x, 3) + Math.Atan(f_x);
-                }
-                else
-                {
-                    d = Math.Pow(y + f_x, 3) + 0.5;
-                }
-
-                ResultBox.Text = d.ToString("F2");
+                ResultBox.Text = "Ошибка: некорректный ввод";
+                return;
             }
-            catch (Exception ex)
+
+            double f_x = 0;
+
+            // Выбор функции
+            if (FuncSh.IsChecked == true)
             {
-                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                f_x = Math.Sinh(x);
             }
+            else if (FuncX2.IsChecked == true)
+            {
+                f_x = Math.Pow(x, 2);
+            }
+            else if (FuncExp.IsChecked == true)
+            {
+                f_x = Math.Exp(x);
+            }
+
+            // Вычисление d
+            double d;
+            if (x > y)
+            {
+                d = Math.Pow(f_x - y, 3) + Math.Atan(f_x);
+            }
+            else if (x < y)
+            {
+                d = Math.Pow(y - f_x, 3) + Math.Atan(f_x);
+            }
+            else
+            {
+                d = Math.Pow(y + f_x, 3) + 0.5;
+            }
+
+            // Обновление текстового поля с результатом
+            ResultBox.Text = d.ToString("F2");
         }
+
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
             InputX.Clear();
             InputY.Clear();
             ResultBox.Clear();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Уверены, что хотите выйти из приложения?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.No) 
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
